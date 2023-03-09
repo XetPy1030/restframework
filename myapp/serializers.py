f817a8b54651e5c2c5cf3aee1c377dbb684b8483
XetPy1030 <xetpy1030@mail.ru>
2023-03-09 12:13:16 +0300

    blank

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,9 +1,2 @@
 from rest_framework import serializers
 
-from myapp.models import Book
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,5 @@
 from django.urls import path
 
-from .views import books, edit_book, delete_book, create_book
-
 urlpatterns = [
-    path("books/", books, name="books"),
-    path("books/create/", create_book, name="create_book"),
-    path("books/edit/<int:pk>/", edit_book, name="edit_book"),
-    path("books/delete/<int:pk>/", delete_book, name="delete_book"),
+
 ]
