981a423ac30805c98de361571dc9779a17a3aca8
XetPy1030 <xetpy1030@mail.ru>
2023-03-11 22:28:02 +0300

    fix

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -9,13 +9,68 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class ProductSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    category = serializers.CharField(max_length=255)
+    manufacturer = serializers.CharField(max_length=255)
+    price = serializers.FloatField()
+    description = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.category = validated_data.get('category', instance.category)
+        instance.manufacturer = validated_data.get('manufacturer', instance.manufacturer)
+        instance.price = validated_data.get('price', instance.price)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
+
+    def save(self):
+        return self.create(self.validated_data)
+
+
 class ManufacturerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manufacturer
         fields = "__all__"
 
 
+class ManufacturerSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+    country = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Manufacturer.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.country = validated_data.get('country', instance.country)
+        instance.save()
+        return instance
+
+    def save(self):
+        return self.create(self.validated_data)
+
+
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
+
+
+class CategorySerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+    def save(self):
+        return self.create(self.validated_data)
